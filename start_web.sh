b5656d7f4a74664df844daeeda8b2a7678b5bf28
Omena0 <omena0mc@gmail.com>
2026-05-13 16:00:22 +0300

    remove serve scripts

--- start_web.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Quick start script for web client development
-
-echo "Starting web client server on http://localhost:8080"
-echo "Make sure your game server is running on port 8081"
-echo ""
-python3 serve_web.py
